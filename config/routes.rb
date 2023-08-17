5af50604cf7929fa1da09ff06dde2eea53fa0094
Edmilton Oliveira <edmilton.ro@hotmail.com>
2023-08-17 09:24:53 -0300

    chore: create route and initialization to rswag ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   namespace :v1 do
-    mount Rswag::Api::Engine => '/api-docs'
     resources :vagas, only: %i[index create]
     resources :pessoas, only: %i[index create]
     resources :candidaturas, only: %i[index create]

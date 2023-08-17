2099a7364cbebf7ee84ca9a47e144af2510f034f
Edmilton Oliveira <edmilton.ro@hotmail.com>
2023-08-17 15:21:35 -0300

    chore: update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
   namespace :v1 do
-    resources :vagas, only: %i[index create]
-    resources :pessoas, only: %i[index create]
-    resources :candidaturas, only: %i[index create]
-    get '/vagas/:id/candidaturas/ranking', to: 'candidaturas#ranking'
+    resources :vagas, only: %i[index show create] do
+      get '/candidaturas/ranking', to: 'candidaturas#ranking'
+    end
+    resources :pessoas, only: %i[index show create] do
+      resources :linguas, only: %i[index show create]
+    end
+    resources :candidaturas, only: %i[index show create]
+    resources :idiomas, only: %i[index show create]
+    # get '/vagas/:id/candidaturas/ranking', to: 'candidaturas#ranking'
   end
 end

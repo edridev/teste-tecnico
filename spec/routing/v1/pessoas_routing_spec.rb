bfc91af4d39db61d96f467818167be52cbb70317
Edmilton Oliveira <edmilton.ro@hotmail.com>
2023-08-17 15:21:54 -0300

    chore: remove routes test

--- spec/routing/v1/pessoas_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe V1::PessoasController, type: :routing do
-  describe 'routing' do
-    it 'routes to #create' do
-      should route(:post, '/v1/pessoas').to('v1/pessoas#create')
-    end
-  end
-end
--- spec/routing/v1/vagas_routing_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe V1::VagasController, type: :routing do
-  describe 'routing' do
-    it 'routes to #create' do
-      should route(:post, '/v1/vagas').to('v1/vagas#create')
-    end
-  end
-end
